f17cce5c74b14f76e802121d31dfb9d79cdf05fd
enagora <info@enagora.com>
2016-10-31 21:41:29 +0100

    XF.B-6
    Ejemplo de como usar un listview mediante codigo C#

---
+++ XF.B-6 ListViews/ListViews/ListViews/App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViews
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // El objeto que va a tener nuestras tareas de TodoItem
+
+
+            // Creamos la pagina principal
+            MainPage = new ListViews.HomePage();
+            
+        }
+        
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XF.B-6 ListViews/ListViews/ListViews/HomePage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ListViews
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            ListView listView = new ListView();
+
+            // Introducimos unos valores para el ejemplo
+            listView.ItemsSource = new TodoItem[]
+            {
+                new TodoItem {Name="comprar pan" },
+                new TodoItem {Name="comprar manzanas" },
+                new TodoItem {Name="comprar peras", Done=true },
+                new TodoItem {Name="comprar azucar" , Done=true },
+                new TodoItem {Name="completar tutorial" }
+            };
+
+            // asignamos el tipo de dato que vamos a relacionar con la plantilla
+            listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Children = {
+                        listView
+                    }
+
+            };
+            
+
+            // Nos suscribimos al evento clic para poder actuar cuando se seleccione
+            // un elemento de la lista
+            listView.ItemSelected += async (sender, e) =>
+            {
+                await DisplayAlert("Has tocado!", e.SelectedItem + "seleccionado!", "OK");
+            };
+        }
+    }
+}
