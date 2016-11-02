b8898f61b336c977d86e37062e02d7e15de5955d
enagora <info@enagora.com>
2016-11-02 07:32:02 +0100

    XF.B-7 Celdas personalizadas
    Codigo fuente del tutorial https://www.enagora.com/?p=70

---
+++ XF.B-7 Celdas personalizadas/CeldasPersonalizadas/CeldasPersonalizadas/HomePage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CeldasPersonalizadas
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            Title = "Celdas personalizadas";
+
+            List<Juego> listaJuegos = Juego.ObtenerListaJuegos();
+            var listview = new ListView { RowHeight = 80 };
+            listview.ItemsSource = listaJuegos;
+            listview.ItemTemplate = new DataTemplate(typeof(JuegoCell));
+            Content = new StackLayout
+            {
+                Children = {
+                   listview
+                }
+            };
+        }
+        
+    }
+
+}
---
+++ XF.B-7 Celdas personalizadas/CeldasPersonalizadas/CeldasPersonalizadas/JuegoCell.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CeldasPersonalizadas
+{
+    class JuegoCell: ViewCell
+    {
+        public JuegoCell()
+        {
+            var imagen = new Image
+            {
+                HorizontalOptions = LayoutOptions.Start
+            };
+            imagen.SetBinding(Image.SourceProperty, new Binding("ImageUri"));
+            imagen.WidthRequest = imagen.HeightRequest = 80;
+
+            var capaNombre = CrearCapaNombre();
+            var capaVista = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { imagen, capaNombre }
+            };
+
+            View = capaVista;
+        }
+
+        static StackLayout CrearCapaNombre()
+        {
+            var etiquetaNombre = new Label
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            etiquetaNombre.SetBinding(Label.TextProperty, "Titulo");
+
+            var etiquetaConsola = new Label
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand
+
+            };
+            etiquetaConsola.SetBinding(Label.TextProperty, "Plataforma");
+
+            var capaNombre = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                Orientation = StackOrientation.Vertical,
+                Children = { etiquetaNombre, etiquetaConsola }
+            };
+            return capaNombre;
+
+        }
+    }
+}
