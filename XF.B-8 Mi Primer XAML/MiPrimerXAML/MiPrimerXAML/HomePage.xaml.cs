e42bdeb3fa96d55c3bed1b2a32c847fcf2c351eb
enagora <info@enagora.com>
2016-11-02 21:09:57 +0100

    XF.B-8 Mi Primer XAML
    https://www.enagora.com/?p=63

---
+++ XF.B-8 Mi Primer XAML/MiPrimerXAML/MiPrimerXAML/HomePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MiPrimerXAML
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            listView.ItemsSource = new TodoItem[]
+           {
+                new TodoItem {Name="comprar pan" },
+                new TodoItem {Name="comprar manzanas" },
+                new TodoItem {Name="comprar peras", Done=true },
+                new TodoItem {Name="comprar azucar" , Done=true },
+                new TodoItem {Name="completar tutorial" }
+           };
+        }
+    }
+}
