78ac2c2bf9603f562579512dc3542040eca39e2d
enagora <info@enagora.com>
2016-11-07 22:27:26 +0100

    XF.B-9 Celdas personalizadas con XAML
    Codigo fuente del tutorial  https://enagora.com/?p=90

---
+++ XF.B-9 Celdas personalizadas con XAML/CeldasPersonalizadasXAML/CeldasPersonalizadasXAML/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CeldasPersonalizadasXAML
+{
+    public partial class HomePage : ContentPage
+    {
+
+        public static ObservableCollection<Juego> ListaJuegos { get; set; }
+
+        public HomePage()
+        {
+            ListaJuegos = new ObservableCollection<Juego>(Juego.ObtenerListaJuegos());
+            InitializeComponent();
+            
+
+        }
+    }
+}
---
+++ XF.B-9 Celdas personalizadas con XAML/CeldasPersonalizadasXAML/CeldasPersonalizadasXAML/Juego.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeldasPersonalizadasXAML
+{
+    public class Juego
+    {
+        public string ImageUri { get; set; }
+        public string Titulo { get; set; }
+        public string Plataforma { get; set; }
+
+        public static List<Juego> ObtenerListaJuegos()
+        {
+            var listaJuegos = new List<Juego>
+            {
+                new Juego
+                {
+                    Plataforma="Xbox One",
+                    Titulo= "Rise of Tomb Raider",
+                    ImageUri="https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTccmFKnirDv3gNn.Z.6MbRLYn4LUy4if1SwSqQge.I3jvpEUmippmEsV7.iCCCPrpv3zKRymwqb0UNZ5SCqGiCXH.F5A99JRKf6tNgIxO57J_64Oxzx3hl.y48oC2XIQAamkoy_UwnTAr80dF8WJjFf_pT.DrLc8VZow8oPyQ34og-"
+                },
+                new Juego
+                {
+                    Plataforma="PS4",
+                    Titulo="Fifa 17",
+                    ImageUri="https://media.game.es/COVERV2/3D_L/127/127809.png"
+                },
+                new Juego
+                {
+                    Plataforma="PC",
+                    Titulo ="Mafia III",
+                    ImageUri="https://media.game.es/COVERV2/3D_L/131/131102.png"
+                }
+
+            };
+
+            return listaJuegos;
+        }
+    }
+}
